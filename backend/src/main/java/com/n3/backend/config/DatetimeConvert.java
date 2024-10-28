9cf6fb94566817e65c2f38c59a705bab3e42f4f4
quang.tx <xq01022002@gmail.com>
2024-10-28 14:06:43 +0700

    DatetimeConvert

---
+++ backend/src/main/java/com/n3/backend/config/DatetimeConvert.java
@@ -0,0 +1,21 @@
+package com.n3.backend.config;
+
+import java.sql.Timestamp;
+
+public class DatetimeConvert {
+    public static String timastampToString(Timestamp date) {
+        return (date != null) ? date.toLocalDateTime().toString() : "";
+    }
+
+    public static Timestamp stringToTimastamp(String date) {
+        return (!date.isEmpty()) ? Timestamp.valueOf(date) : null;
+    }
+
+    public static String DateToString(java.sql.Date date) {
+        return (date != null) ? date.toLocalDate().toString() : "";
+    }
+
+    public static java.sql.Date stringToDate(String date) {
+        return (!date.isEmpty()) ? java.sql.Date.valueOf(date) : null;
+    }
+}
