16930ef32ddc2f3593c3fcce33355c8a20317222
quang.tx <xq01022002@gmail.com>
2024-10-28 14:06:22 +0700

    custom exception

---
+++ backend/src/main/java/com/n3/backend/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package com.n3.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.util.Map;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        Map<String, Object> data = Map.of(
+                "status", false,
+                "resposeCode", 403,
+                "message", "Access Denied"
+        );
+        response.getOutputStream().println((new ObjectMapper().writeValueAsString(data)));
+        response.setStatus(403);
+    }
+}
---
+++ backend/src/main/java/com/n3/backend/config/CustomAuthenticationPoint.java
@@ -0,0 +1,27 @@
+package com.n3.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+public class CustomAuthenticationPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        Map<String, Object> data = Map.of(
+                "status", false,
+                "resposeCode", 401,
+                "message", "Unauthorized"
+        );
+        response.getOutputStream().println((new ObjectMapper().writeValueAsString(data)));
+        response.setStatus(401);
+    }
+}
