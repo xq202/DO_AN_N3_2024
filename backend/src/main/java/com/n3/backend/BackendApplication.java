164767f3875e624d0faa0a4a4b496b105804f3e9
quang.tx <xq01022002@gmail.com>
2024-12-22 02:43:46 +0700

    fix timezone

--- backend/src/main/java/com/n3/backend/BackendApplication.java
+++ backend/src/main/java/com/n3/backend/BackendApplication.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @RestController
 public class BackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(BackendApplication.class, args);
     }
 
