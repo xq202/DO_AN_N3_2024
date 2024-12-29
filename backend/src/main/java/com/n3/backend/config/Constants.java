445c54b317fe38ed3b6ad4c7d4beae193d9fb342
quang.tx <xq01022002@gmail.com>
2024-12-29 22:04:02 +0700

    edit url fe

--- backend/src/main/java/com/n3/backend/config/Constants.java
+++ backend/src/main/java/com/n3/backend/config/Constants.java
@@ -1,5 +1,5 @@
 package com.n3.backend.config;
 
 public class Constants {
-    public static final String FeUrl = "https://webn3.qtxhost.me";
+    public static final String FeUrl = "https://thesis-fe-awr4.vercel.app/buy";
 }
